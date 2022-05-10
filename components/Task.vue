9e9d4f4a0cda977717f230a2e7a77d4aafecdee0
catalin.festila <catafest76@gmail.com>
2022-05-10 12:17:31 +0300

    first commit

---
+++ components/Task.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="`task ${task.done ? 'is-complete' : ''}`">
+    <div class="content">{{ task.content }}</div> 
+    <div class="buttons">
+      <button @click="toggleDone">
+        {{ task.done ? 'Undo' : 'Done' }}
+      </button>
+      <button @click="removeTask" class="delete">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // this will add the task on browser 
+  props: ['task'],
+  methods: {
+    toggleDone(task) {
+      this.$store.commit('TOGGLE_TASK', this.task);
+    },
+    removeTask(task) {
+      this.$store.commit('REMOVE_TASK', this.task);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- <div>Hello, World!</div> -->
+  <!-- layout the Application -->
+  <main>
+    <h1>Task board</h1>
+    <p>Create a list of tasks</p>
+    <div class="create-new">
+      <!-- keypress on Enter key to create a new task-->
+      <input type="text" v-model="newTask" placeholder="Add a new task" @keypress.enter="addTask" />
+      <!-- add button with @click event  -->
+      <button @click="addTask">Add</button>
+    </div>
+    <div class="tasks">
+      <!-- task component -->
+      <Task v-for="(task,i) in $store.state.tasks"
+      :key="i"
+      :task="task" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newTask: ''
+    }  
+  },
+  methods: {
+    addTask () {
+      if ( this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = '';
+      }
+    }
+  }
+}
+</script>
